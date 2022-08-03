5a1cbe154c5c50cce8d461d5f456a567c5e4a9ef
Zasada94 <pawelzasada2@wp.pl>
2022-08-03 13:25:33 +0200

    sidebar mobile

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const burger = document.querySelector(".burger");
 
 const iconBurger = document.querySelector(".fa-bars");
-const iconX = document.querySelector(".fa-times");
+const iconX = document.querySelector(".fa-solid.fa-xmark");
 const column = document.querySelector("aside");
 
 burger.addEventListener("click", function () {
