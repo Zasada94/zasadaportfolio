ca65d30c38949c5719fb3719a58f312f943eb288
Zasada94 <pawelzasada2@wp.pl>
2022-08-10 14:40:13 +0200

    header shadow on scroll added

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const burger = document.querySelector(".burger");
-
 const iconBurger = document.querySelector(".fa-bars");
 const iconX = document.querySelector(".fa-solid.fa-xmark");
 const column = document.querySelector("aside");
@@ -8,4 +7,14 @@ burger.addEventListener("click", function () {
 	iconBurger.classList.toggle("show");
 	iconX.classList.toggle("show");
 	column.classList.toggle("show");
+});
+
+const header = document.querySelector(".headerContainer");
+
+document.addEventListener("scroll", function () {
+	if (window.pageYOffset == 0) {
+		header.classList.remove("is-scroll");
+	} else {
+		header.classList.add("is-scroll");
+	}
 });
