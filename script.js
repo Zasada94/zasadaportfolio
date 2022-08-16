50e2216db88c7b8e26abf224d17e27c2303e39ba
Zasada94 <pawelzasada2@wp.pl>
2022-08-16 13:42:16 +0200

    about me section finished

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const burger = document.querySelector(".burger");
 const iconBurger = document.querySelector(".fa-bars");
 const iconX = document.querySelector(".fa-solid.fa-xmark");
 const column = document.querySelector("aside");
+const menuItems = document.querySelectorAll(".menuItem");
 
 burger.addEventListener("click", function () {
 	iconBurger.classList.toggle("show");
@@ -17,4 +18,12 @@ document.addEventListener("scroll", function () {
 	} else {
 		header.classList.add("is-scroll");
 	}
+});
+
+menuItems.forEach((item) => {
+	item.addEventListener("click", () => {
+		iconBurger.classList.toggle("show");
+		iconX.classList.toggle("show");
+		column.classList.toggle("show");
+	});
 });
