471935b801c7081660ec2ba37b0909ccdda689e1
Zasada94 <pawelzasada2@wp.pl>
2023-08-02 12:09:58 +0200

    added preloader to videos

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+//preloader
+const preloader = document.getElementById("preloader");
+const wholeWrapper = document.getElementById("wholeWrapper");
+
+const queue = new createjs.LoadQueue();
+
+queue.loadFile("./video/actimel.mp4");
+queue.loadFile("./video/blockbreaker.mp4");
+queue.loadFile("./video/heinz.mp4");
+queue.loadFile("./video/pandora.mp4");
+queue.loadFile("./video/winiary.mp4");
+queue.loadFile("./video/worldcup.mp4");
+
+queue.on("complete", function () {
+	preloader.style.display = "none";
+	wholeWrapper.style.display = "block";
+});
+
 const burger = document.querySelector(".burger");
 const column = document.querySelector("aside");
 const menuItems = document.querySelectorAll(".menuItem");
